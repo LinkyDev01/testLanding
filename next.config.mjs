6cf267b87dbb24d072d7ff4065e95b55c7644066
JK <74600256+Programmer-JK@users.noreply.github.com>
2026-03-20 17:35:00 +0900

    change : redirct 수정

--- next.config.mjs
+++ next.config.mjs
@@ -6,6 +6,15 @@ const nextConfig = {
   images: {
     unoptimized: true,
   },
+  async redirects() {
+    return [
+      {
+        source: "/lounge",
+        destination: "https://linkylounge.com/",
+        permanent: false,
+      },
+    ]
+  },
 }
 
 export default nextConfig
